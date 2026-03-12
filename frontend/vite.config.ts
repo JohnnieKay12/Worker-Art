1d8a92c3481cc2f252e6a115716947ff9c0fa48a
JohnnieKay12 <142480261+JohnnieKay12@users.noreply.github.com>
2026-03-12 09:51:35 +0100

    Fix vercel build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,6 +7,9 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 export default defineConfig({
   base: './',
   plugins: [inspectAttr(), react()],
+  build:{
+    sourcemap: false
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
